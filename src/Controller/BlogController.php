78d3c3ba5d1a68ddaa014e1a9b85d39d5488c7a3
A-Saleh <salehanbari@gmail.com>
2022-04-09 17:47:32 +0430

    homepage & read more

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,20 +2,52 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Post;
+use App\Form\CommentType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
-    #[Route('/blog', name: 'app_blog')]
+    #[Route('/', name: 'homepage')]
     public function index(ManagerRegistry $doctrine): Response
     {
         $posts = $doctrine->getRepository(Post::class)->getAllPosts();
         return $this->render('blog/index.html.twig', [
             "posts" => $posts
+        ]);
+    }
+
+    /**
+     * @Route("/article-{id}", name="read_post")
+//     * @param User|null $user
+     * @param Post $post
+     * @param Request $request
+     * @param ManagerRegistry $doctrine
+     * @return Response
+     */
+    public function read(Post $post, Request $request, ManagerRegistry $doctrine): Response
+    {
+        $comment = new Comment();
+//        $comment->setAuthor($user->getUserIdentifier());
+        $comment->setPost($post);
+
+        $form = $this->createForm(CommentType::class, $comment)->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $doctrine->getManager()->persist($comment);
+            $doctrine->getManager()->flush();
+
+            return $this->redirectToRoute("read_post", ["id" => $post->getId()]);
+        }
+
+        return $this->render("blog/read.html.twig", [
+            "post" => $post,
+            "form" => $form->createView()
         ]);
     }
 }
